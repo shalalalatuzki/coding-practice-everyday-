1fcfab94142e5ddfb54e445846e4fe246f2f98f8
shalalalatuzki <2550662608@qq.com>
2018-11-19 18:46:19 +0800

    structure testOne

---
+++ structureTestOne/structureTestOne/findRange.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int S[10000001];
+
+int main() {
+	int size, queries;
+	cin >> size >> queries;
+	int a, b;
+
+	for (int i = 0; i < size; i++) {
+		cin >> S[i];
+	}
+	for (int i = 0; i < queries; i++) {
+		cin >> a >> b;
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (S[i] <= b && S[i] >= a) count++;
+		}
+		cout << count << endl;
+	}
+	
+	return 0;
+}
