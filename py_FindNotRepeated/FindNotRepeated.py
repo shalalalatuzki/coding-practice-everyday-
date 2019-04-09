25ec5ae01a0558499875f6906eab7a9a7516a173
shalalalatuzki <2550662608@qq.com>
2019-04-10 00:22:23 +0800

    submit code

---
+++ py_FindNotRepeated/FindNotRepeated.py
@@ -0,0 +1,14 @@
+def FindNotRepeated(arr,n):
+    mp=dict()
+    for i in range(n):
+        mp[arr[i]]+=1
+    for i in range(n):
+         if mp[arr[i]]==1:
+             return arr[i]
+    return -1
+
+arr=[-1,2,-1,3,2]
+n=len(arr)
+print(FindNotRepeated(arr,n)
+
+
